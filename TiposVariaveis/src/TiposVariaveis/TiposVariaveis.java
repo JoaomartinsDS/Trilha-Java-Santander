7c33b3d440edece228629a2b951b73e802dea6f0
João Batista <joaobmds@outlook.com>
2024-04-24 23:43:13 -0300

    Finalização dos tipos e variaveis

--- TiposVariaveis/src/TiposVariaveis/TiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis/TiposVariaveis.java
@@ -1,5 +1,11 @@
 package TiposVariaveis;
 
 public class TiposVariaveis {
+	public static  void main(String [] args){
+		int idade =10;
+		String nome = "joao";
+		double preco = 20.99;
+		System.out.print(preco);
+	}
 
 }
