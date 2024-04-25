10962b30d35dacb31a951d7a209ef732f06366c5
João Batista <joaobmds@outlook.com>
2024-04-24 23:47:16 -0300

    Correção da finalização dos tipos e variaveis

--- TiposVariaveis/src/TiposVariaveis/TiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis/TiposVariaveis.java
@@ -2,10 +2,22 @@ package TiposVariaveis;
 
 public class TiposVariaveis {
 	public static  void main(String [] args){
-		int idade =10;
-		String nome = "joao";
-		double preco = 20.99;
-		System.out.print(preco);
+		//TIPOS
+		//int idade =10;
+		//String nome = "joao";
+		//double preco = 20.99;
+		//System.out.print(preco);
+		//short numeroCurto = 1;
+		//int numeroNormal = numeroCurto;
+		//REALIZANDO CASTING
+		//short numeroCurto2 = (short) numeroNormal;
+		
+		//VARIAVEL
+		//int numero = 5;
+		//numero = 10;
+		//CONSTANTE
+		//final double PI = 3.14;
+		//PI = 10;
 	}
 
 }
